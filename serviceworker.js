cc640b1ed569bacd2dab4d830ac72a956f5c90a3
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2021-03-08 10:51:42 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,4 @@
+/*
 const CACHE_NAME = "version-1";
 const urlsToCache = [ '/index.html' , '/noInternet.html', '/css/style.css', '/images/404_orange.png'];
 
@@ -41,4 +42,55 @@ self.addEventListener('activate', (event) => {
         ))
             
     )
+});
+*/
+
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "noInternet.html";
+const urlsToCache = ['/noInternet.html', '/css/style.css', '/images/404_orange.png'];
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(urlsToCache)) //cache.add(offlineFallbackPage)
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
