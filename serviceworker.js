5077a7649496a80c1e38fab8acbd3f45f4fff44e
jovanblazek <36740941+jovanblazek@users.noreply.github.com>
2021-03-09 11:13:58 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -134,26 +134,51 @@ self.addEventListener("fetch", function (event) {
 
 const CACHE_NAME = "ehub-pwa-v1";
 //const assets = ["/", "/index.html", "/css/style.css", "/noInternet.html", "/images/404_orange.png"];
-const assets = ["/css/style.css", "/noInternet.html", "/images/404_orange.png"];
+const assets = [
+	"/css/style.css", 
+	"/noInternet.html", 
+	"/images/404_orange.png",
+	"/images/icons/android-chrome-36x36.png",
+	"/images/icons/android-chrome-48x48.png",
+	"/images/icons/android-chrome-72x72.png",
+	"/images/icons/android-chrome-144x144.png",
+	'/site.webmanifest', 
+	"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+];
+const OFFLINE_URL = "noInternet.html";
 
-self.addEventListener("install", (installEvent) => {
-	installEvent.waitUntil(
+self.addEventListener("install", (event) => {
+	console.log("Attempting to install service worker and cache static assets");
+	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => {
-			cache.addAll(assets);
+			return cache.addAll(assets);
 		})
 	);
 });
 
-self.addEventListener("fetch", (fetchEvent) => {
-	fetchEvent.respondWith(
-		fetch(fetchEvent.request).catch(() => caches.match('/noInternet.html'))
-		/*
-		caches.match(fetchEvent.request).then((res) => {
-			return res || fetch(fetchEvent.request);
-		})*/
-	);
+this.addEventListener("fetch", (event) => {
+	// request.mode = navigate isn't supported in all browsers
+	// so include a check for Accept: text/html header.
+	if (
+		event.request.mode === "navigate" ||
+		(event.request.method === "GET" &&
+			event.request.headers.get("accept").includes("text/html"))
+	) {
+		event.respondWith(
+			fetch(event.request.url).catch((error) => {
+				// Return the offline page
+				return caches.match(OFFLINE_URL);
+			})
+		);
+	} else {
+		// Respond with everything else if we can
+		event.respondWith(
+			caches.match(event.request).then(function (response) {
+				return response || fetch(event.request);
+			})
+		);
+	}
 });
-
 
 /*	WORKING
 
